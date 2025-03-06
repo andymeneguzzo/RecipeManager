423db52e43b93de1bbce62d2282f0c0ec248daa9
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-03-06 22:38:00 +0100

    Add searchBar and searchButton to UI and MainActivity.kt. Create item_recipe.xml

--- app/src/main/java/com/andy/recipemanager/MainActivity.kt
+++ app/src/main/java/com/andy/recipemanager/MainActivity.kt
@@ -2,10 +2,14 @@ package com.andy.recipemanager
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import androidx.core.view.isGone
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,11 +22,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // BOTTONI
+        // BUTTONS
         hamburgerButton = findViewById(R.id.hamburgerButton)
         userButton = findViewById(R.id.userButton)
         addButton = findViewById(R.id.addButton)
         settingsButton = findViewById(R.id.settingsButton)
+
+        // SEARCH
+        val searchButton = findViewById<ImageButton>(R.id.searchButton)
+        val searchBar = findViewById<EditText>(R.id.searchBar)
+
+        searchButton.setOnClickListener {
+            // Toggle if button pressed
+            if(searchBar.isGone) {
+                searchBar.visibility = View.VISIBLE
+
+                // searchBar.requestFocus() -> optional
+            } else {
+                searchBar.visibility = View.GONE
+            }
+        }
 
         val topBar = findViewById<LinearLayout>(R.id.clickableTopBar)
         topBar.setOnClickListener {
---
+++ app/src/main/java/com/andy/recipemanager/data/Recipe.kt
@@ -0,0 +1,4 @@
+package com.andy.recipemanager.data
+
+class Recipe {
+}
