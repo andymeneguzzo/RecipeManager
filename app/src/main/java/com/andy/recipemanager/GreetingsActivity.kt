46c4de550a12e6b2f01508c306f8551f462ff895
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-03-06 13:41:36 +0100

    Add fabHome to GreetingsActivity.kt

--- app/src/main/java/com/andy/recipemanager/GreetingsActivity.kt
+++ app/src/main/java/com/andy/recipemanager/GreetingsActivity.kt
@@ -1,11 +1,22 @@
 package com.andy.recipemanager
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class GreetingsActivity : AppCompatActivity() {
+
+    private lateinit var fabHome: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_greetings)
+
+        fabHome = findViewById(R.id.fab_home)
+
+        fabHome.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
