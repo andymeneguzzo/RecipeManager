a03f30f6e1aa7c80ab7573aaa73764dfc8b6988a
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-03-06 13:19:39 +0100

    Add GreetingsActivity.kt and activity_greetings.xml, refactor colors.xml and strings.xml

--- app/src/main/java/com/andy/recipemanager/MainActivity.kt
+++ app/src/main/java/com/andy/recipemanager/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.andy.recipemanager
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var hamburgerButton: Button
+    private lateinit var userButton: Button
+    private lateinit var addButton: Button
+    private lateinit var settingsButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // BOTTONI
+        hamburgerButton = findViewById(R.id.hamburgerButton)
+        userButton = findViewById(R.id.userButton)
+        addButton = findViewById(R.id.addButton)
+        settingsButton = findViewById(R.id.settingsButton)
+
+
     }
 }
