d8b47260b25dded555428cd4e477b32be08ba906
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-03-07 14:54:51 +0100

    add edit recipies activities

--- app/src/main/java/com/andy/recipemanager/activities/AddRecipeActivity.kt
+++ app/src/main/java/com/andy/recipemanager/activities/AddRecipeActivity.kt
@@ -1,12 +1,25 @@
 package com.andy.recipemanager.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.andy.recipemanager.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class AddRecipeActivity : AppCompatActivity() {
+
+    private lateinit var addButton: FloatingActionButton
+    private lateinit var homeButton: FloatingActionButton
+    private lateinit var settingsButton: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_recipe)
+
+        homeButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
