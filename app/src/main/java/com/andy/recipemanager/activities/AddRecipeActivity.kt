07f0568ea54386ce93c18fb6d0259d0231e25ec7
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-03-07 11:15:29 +0100

    add activity_add_recipe.xml and AddRecipeActivity.kt

---
+++ app/src/main/java/com/andy/recipemanager/activities/AddRecipeActivity.kt
@@ -0,0 +1,12 @@
+package com.andy.recipemanager.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.andy.recipemanager.R
+
+class AddRecipeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_recipe)
+    }
+}
