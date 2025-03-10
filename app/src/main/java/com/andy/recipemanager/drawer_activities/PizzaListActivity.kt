18cdc472b329b482b5fdaca22e10aecddb3e2460
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-03-10 14:56:05 +0100

    list activities

--- app/src/main/java/com/andy/recipemanager/drawer_activities/PizzaListActivity.kt
+++ app/src/main/java/com/andy/recipemanager/drawer_activities/PizzaListActivity.kt
@@ -1,12 +1,23 @@
 package com.andy.recipemanager.drawer_activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.andy.recipemanager.R
+import com.andy.recipemanager.activities.MainActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class PizzaListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_pizza)
+
+        val fabHome = findViewById<FloatingActionButton>(R.id.fab_home)
+
+        fabHome.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
