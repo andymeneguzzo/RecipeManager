86c09fb1b31e2667dd88f5bc9e5056e7be93d290
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-03-11 06:54:02 +0100

    Ui changes

---
+++ app/src/main/java/com/andy/recipemanager/activities/IconsListActivity.kt
@@ -0,0 +1,13 @@
+package com.andy.recipemanager.activities
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.andy.recipemanager.R
+
+class IconsListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_icons)
+    }
+}
