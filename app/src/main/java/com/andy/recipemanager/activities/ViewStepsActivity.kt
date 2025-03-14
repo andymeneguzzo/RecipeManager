f19c3937e1de1fc7ae41dd9ea427cb854d42e68c
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-03-14 11:10:21 +0100

    ...

--- app/src/main/java/com/andy/recipemanager/activities/ViewStepsActivity.kt
+++ app/src/main/java/com/andy/recipemanager/activities/ViewStepsActivity.kt
@@ -2,7 +2,6 @@ package com.andy.recipemanager.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
